2442305838caab717922484bc9ca85d04cb44ad1
Klesya <v.klesya@hotmail.fr>
2024-11-17 00:56:54 +0100

    Initial Commit

---
+++ FacebookTests/FacebookTests.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookTests.swift
+//  FacebookTests
+//
+//  Created by Klesya on 17/11/2024.
+//
+
+import Testing
+@testable import Facebook
+
+struct FacebookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
